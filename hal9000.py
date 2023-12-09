32f6949813aeab9ceec4ddc015535e4889fbda4a
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-12-09 05:04:26 -0400

    Update hal9000.py

--- hal9000.py
+++ hal9000.py
@@ -1,17 +1,21 @@
 # pip install openai
 import openai
+import os
+import sys
 from time import sleep
 from random import randint
+from json import dump
+import readline
 
 
 client = openai.OpenAI(
-    api_key = "INSERT_API_KEY") # Insert your ChatGPT API key here
+    api_key="insertapikey")  # Insert your ChatGPT API key here
 
 
 def corny_intro():
     print("\n" * 40 + "[*] HAL 9000 ~ performing startup checks")
     sleep(1)
-    print('[*] HAL 9000 ~ neural network initialized."')
+    print('[*] HAL 9000 ~ neural network initialized.')
     sleep(1)
     system_msg = input("Who do you need HAL 9000 to be today?\n>> ")
     msgs = [{"role": "system", "content": system_msg}]
@@ -29,16 +33,41 @@ def funny_robot():
         print("\nI'm sorry, Dave. I'm afraid I can't do that.")
         sleep(5)
         print("\nI'm sorry, Dave. That was a joke.")
+        sleep(2)
 
 
-msgs = corny_intro()
-while input != "quit()":
-    message = input("> ")
-    msgs.append({"role": "user", "content": message})
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=msgs)
-    reply = response.choices[0].message.content
-    msgs.append({"role": "assistant", "content": reply})
-    funny_robot()
-    print(f"\n{reply}\n")
+def init_log():
+    with open('./logs/log-number.txt', 'r') as f:
+        log_number = int(f.read())
+    log_number += 1
+    with open('./logs/log-number.txt', 'w') as f:
+        f.write(str(log_number))
+    new_log = f'./logs/log{log_number}.txt'
+    with open(new_log, 'x') as f:
+        pass
+    return  new_log
+
+
+def log_conversation(_logfile, messages):
+    with open(_logfile, 'w') as f:
+        dump(messages, f, indent=6)
+
+
+if __name__ == '__main__':
+    try:
+        logfile = init_log()
+        msgs = corny_intro()
+        while input != "quit()":
+            message = input("> ")
+            msgs.append({"role": "user", "content": message})
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=msgs)
+            reply = response.choices[0].message.content
+            msgs.append({"role": "assistant", "content": reply})
+            funny_robot()
+            print(f"\n{reply}\n")
+            log_conversation(logfile, msgs)
+    except Exception as e:
+        print(e)
+        sys.exit()
